9076c42514a90a4a6d3335af2bfb586195b63b80
Julius Macrohon <julius.karl.macrohon@gmail.com>
2019-07-29 12:43:43 +1200

    initial commit

--- src/SignUp.js
+++ src/SignUp.js
@@ -4,6 +4,30 @@ import logoImg from "./resources/logo.png"
 class SignUp extends Component {
     constructor() {
         super()
+        this.state = {
+            username: '',
+            password: '',
+            email: ''
+        }
+        this.register = this.register.bind(this)
+    }
+
+    register(e) {
+        e.preventDefault()
+        const account = {
+            username: this.state.username,
+            password: this.state.password,
+            email: this.state.email
+        }
+        /*fetch('mongodb://public:publicpass1234@ds113046.mlab.com:13046/mean_project_database/collections/accounts', {
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(account)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))*/
     }
 
     render() {
@@ -33,7 +57,7 @@ class SignUp extends Component {
                     />
                     <br/>
                     <input 
-                        type="text" 
+                        type="password" 
                         id="password" 
                         name="password" 
                         placeholder="Password" 
@@ -41,15 +65,16 @@ class SignUp extends Component {
                     />
                     <br/>
                     <input 
-                        type="text" 
-                        id="password" 
-                        name="password" 
+                        type="password" 
+                        id="confirm-password" 
+                        name="confirm-password" 
                         placeholder="Confirm Password" 
                         className="full-width main-text"
                     />
                     <br/>
                     <button 
-                        className="self-center main-text cursor-pointer">
+                        className="self-center main-text cursor-pointer"
+                        onClick={this.register}>
                         Register
                     </button>
                     <br/>
